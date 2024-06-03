709ca79ea475d3539c86743b3dfaf260268ace08
eullergomes <eullertexeira@gmail.com>
2024-06-03 15:07:33 -0300

    chore: add ESLint and Prettier

--- navigation.ts
+++ navigation.ts
@@ -1,8 +1,8 @@
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 import { languages } from './constants/language';
- 
+
 export const locales = languages;
-export const localePrefix = 'always'; // Default
- 
+export const localePrefix = 'always';
+
 export const { Link, redirect, usePathname, useRouter } =
-  createSharedPathnamesNavigation({locales, localePrefix});
+  createSharedPathnamesNavigation({ locales, localePrefix });
