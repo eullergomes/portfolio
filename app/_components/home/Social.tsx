b8660b986504c07ad1dee6dd54d4c57d9a3ef7a3
eullergomes <eullertexeira@gmail.com>
2024-06-04 11:41:55 -0300

    feat: add Data, ScrollDown and Social components

---
+++ app/_components/home/Social.tsx
@@ -0,0 +1,34 @@
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/eullergomes/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/eullergomes"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+
+      <a
+        href="https://instagram.com/eullertz"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
